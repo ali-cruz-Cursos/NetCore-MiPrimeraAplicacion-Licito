9c4fa0be614e38e40f32e687b082f671261fc458
Ali Cruz <ali.cruz@me.com>
2021-11-25 18:58:28 -0600

    Avance ejercicio 1

---
+++ MiPrimeraAplicacionEnNetCore/Clases/PaginaCLS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiPrimeraAplicacionEnNetCore.Clases
+{
+    public class PaginaCLS
+    {
+        [Display(Name = "ID Pagina")]
+        public int idPagina { get; set; }
+
+        [Display(Name = "Mensaje")]
+        public string mensaje { get; set; }
+
+        [Display(Name = "Accion")]
+        public string accion { get; set; }
+
+        [Display(Name = "Controlador")]
+        public string controlador { get; set; }
+
+
+    }
+}
